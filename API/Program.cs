2a232ff8069b82125ebd012ed7d69c28b836092e
Natalia Polak <n.polak138@gmail.com>
2025-06-21 12:26:22 +0200

    3rd section

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,17 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
+builder.Services.AddCors();
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:3000", "https://localhost:3000" ));
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
